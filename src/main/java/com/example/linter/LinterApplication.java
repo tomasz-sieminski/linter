4e9a170e190008df63eb34e566af810c4dad55e3
Tomasz Sieminski <tomasz.sieminski@allegro.pl>
2022-01-04 15:40:54 +0100

    Initial commit

---
+++ src/main/java/com/example/linter/LinterApplication.java
@@ -0,0 +1,13 @@
+package com.example.linter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/linter/LinterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.linter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
