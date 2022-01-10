21887460782e39d2c9ba85c524e641c60dba3562
Tomasz Sieminski <tomasz.sieminski@allegro.pl>
2022-01-10 12:44:16 +0100

    mleko

--- src/main/java/com/example/linter/EmptyClass.java
+++ src/main/java/com/example/linter/EmptyClass.java
@@ -1,5 +1,11 @@
 package com.example.linter;
 
+
+import org.springframework.boot.admin.*;
+
 public class EmptyClass {
     private String wrong_cases = "test";
+    EmptyClass() {
+        if(wrong_cases.length()!=0) System.out.println("test");
+    }
 }
