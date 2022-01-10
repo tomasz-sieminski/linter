61b07c621647366575b5634c96546c9875820f83
Tomasz Sieminski <tomasz.sieminski@allegro.pl>
2022-01-10 12:50:32 +0100

    mleko

--- src/main/java/com/example/linter/EmptyClass.java
+++ src/main/java/com/example/linter/EmptyClass.java
@@ -2,6 +2,7 @@ package com.example.linter;
 
 
 import org.springframework.boot.admin.*;
+import org.springframework.aop.aspectj.AbstractAspectJAdvice;
 
 public class EmptyClass {
     private String wrong_cases = "test";
