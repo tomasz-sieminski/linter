7a90616fc212ea1551340deb4744bec68978b949
Tomasz Sieminski <tomasz.sieminski@allegro.pl>
2022-01-12 08:26:31 +0100

    testing/Branch

---
+++ src/main/java/com/example/linter/cokolWiek.java
@@ -0,0 +1,10 @@
+package com.example.linter;
+
+import org.*;
+
+public class cokolWiek
+{ private final String TESt123="test";
+private String lineThatIsTooLongShouldBeMarkedAsSuch="this line should be too long and marked as such in pr review";
+
+cokolWiek(){for(int i=0;i<100;i++);}
+}
